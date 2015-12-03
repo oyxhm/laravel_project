61234cb2dc33ffa352f0c0e0aa10be1ca7968669
oyxhm <oyxhmmm@163.com>
2015-12-04 00:27:27 +0800

    use select2

--- resources/views/article/_form.blade.php
+++ resources/views/article/_form.blade.php
@@ -16,9 +16,37 @@
 
 <div class="form-group">
 	{!! Form::label('tag_list','Tags:') !!}
-	{!! Form::select('tag_list[]',$tags,null,['class' => 'form-control','multiple']) !!}
+	{!! Form::select('tag_list[]',$tags,null,['id' => 'tag_list' ,'class' => 'form-control','multiple']) !!}
 </div>
 
 <div class="form-group">
 	{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
 </div>
+
+@section('footer')
+	<script type="text/javascript">
+		$('#tag_list').select2({
+			placeholder : '请选择标签',
+			// tags : true
+			/*ajax : {
+				dataType :　'json',
+				url : 'api/tags',
+				delay : 250,
+				data : function (params) {
+					return {
+						q :  params.term
+					}
+				},
+				processResults:function	(data) {
+					return {
+						results:data
+					}
+				}
+			}*/
+			/*data :　[
+			{id:'one' , text : 'jQuery'},
+			{id:'one' , text : 'Ruby'},
+			]*/
+		});
+	</script>
+@endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
 </head>
 <body>
     <div class="container">
@@ -13,8 +14,10 @@
     
 <script src="http://apps.bdimg.com/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
 <script type="text/javascript">
 	$('div.alert').not('.alert-important').delay(3000).slideUp(300);
 </script>
+@yield('footer')
 </body>
 </html>
